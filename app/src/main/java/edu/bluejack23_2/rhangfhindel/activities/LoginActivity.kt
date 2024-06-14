7744dd593894e9101f1d646f557664627b7c24f7
Daniel Adamlu <adamludnl@gmail.com>
2024-06-14 22:03:43 +0700

    feat: added login ui

--- app/src/main/java/edu/bluejack23_2/rhangfhindel/activities/LoginActivity.kt
+++ app/src/main/java/edu/bluejack23_2/rhangfhindel/activities/LoginActivity.kt
@@ -3,10 +3,21 @@ package edu.bluejack23_2.rhangfhindel.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.bluejack23_2.rhangfhindel.R
+import android.os.Build
+import android.view.Window
+import android.view.WindowManager
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        changeStatusBarColor()
+    }
+
+    private fun changeStatusBarColor() {
+        val window: Window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = this.resources.getColor(R.color.primary_color)
     }
 }
