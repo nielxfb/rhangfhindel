f42981977f2cf759e2f6baf70a95080ad73ead0a
Daniel Adamlu <adamludnl@gmail.com>
2024-06-18 17:15:21 +0700

    Merge branch 'main' of https://github.com/Acad600-TPA/MOBILE-NL-HH-232

--- app/src/main/java/edu/bluejack23_2/rhangfhindel/fragments/ProfileFragment.kt
+++ app/src/main/java/edu/bluejack23_2/rhangfhindel/fragments/ProfileFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
+import edu.bluejack23_2.rhangfhindel.BuildConfig
 import edu.bluejack23_2.rhangfhindel.R
+import edu.bluejack23_2.rhangfhindel.databinding.FragmentProfileBinding
+import edu.bluejack23_2.rhangfhindel.utils.SharedPrefManager
+import edu.bluejack23_2.rhangfhindel.viewmodels.ProfileViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +29,8 @@ class ProfileFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var viewModel: ProfileViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,8 +43,26 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return init(inflater, container)
+    }
+
+    fun init(inflater: LayoutInflater, container: ViewGroup?) : View {
+        val binding: FragmentProfileBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_profile, container, false
+        )
+
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        viewModel.init(requireContext())
+
+        Picasso.get().load(
+            viewModel.profileURL
+        ).into(binding.profileImageView)
+
+        binding.profileFragmentName.text = viewModel.name
+        binding.profileFragmentInitial.text = viewModel.initial
+
+        return binding.root
     }
 
     companion object {
