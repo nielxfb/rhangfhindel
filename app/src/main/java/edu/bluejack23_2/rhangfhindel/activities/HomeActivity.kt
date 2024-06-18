d86e9ebf80ad5820813b0f3b2c43018f88fd263d
hendrahartanto <hendrazhuo10@gmail.com>
2024-06-18 13:18:55 +0700

    feat: navigation redirect

--- app/src/main/java/edu/bluejack23_2/rhangfhindel/activities/HomeActivity.kt
+++ app/src/main/java/edu/bluejack23_2/rhangfhindel/activities/HomeActivity.kt
@@ -1,20 +1,27 @@
 package edu.bluejack23_2.rhangfhindel.activities
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.bluejack23_2.rhangfhindel.R
 import edu.bluejack23_2.rhangfhindel.base.BaseActivity
 
 class HomeActivity : BaseActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
         changeStatusBarColor(R.color.light_border_color)
+        init()
     }
 
     override fun init() {
-        TODO("Not yet implemented")
+        val navController = findNavController(R.id.curr_fragment)
+        val navbarView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navbarView.setupWithNavController(navController)
     }
 
     override fun setEvent() {
