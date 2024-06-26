d560c027f3ca6d93ae4b17b21ae36a8b51a5e153
Daniel Adamlu <adamludnl@gmail.com>
2024-06-26 21:11:13 +0700

    feat: added new Initial field in model Detail

--- app/src/main/java/edu/bluejack23_2/rhangfhindel/models/Detail.kt
+++ app/src/main/java/edu/bluejack23_2/rhangfhindel/models/Detail.kt
@@ -8,5 +8,6 @@ import kotlinx.parcelize.RawValue
 data class Detail(
     val Campus: String,
     val RoomName: String,
+    val Initial: String?,
     val StatusDetails: @RawValue List<List<StatusDetail>>
 ) : Parcelable
