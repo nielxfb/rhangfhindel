12368a9a0981771e510f52f9082245f1e8d0def5
Daniel Adamlu <adamludnl@gmail.com>
2024-06-29 17:24:41 +0700

    feat: encrypted assistant data in sharedpref

--- app/src/main/java/edu/bluejack23_2/rhangfhindel/utils/SharedPrefManager.kt
+++ app/src/main/java/edu/bluejack23_2/rhangfhindel/utils/SharedPrefManager.kt
@@ -3,17 +3,26 @@ package edu.bluejack23_2.rhangfhindel.utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.google.gson.Gson
 import edu.bluejack23_2.rhangfhindel.BuildConfig
 import edu.bluejack23_2.rhangfhindel.models.Assistant
 
 object SharedPrefManager {
-    private fun getSharedPreferences(context: Context): SharedPreferences {
-        return context.getSharedPreferences(BuildConfig.SHARED_PREF_KEY, Context.MODE_PRIVATE)
+    private fun getEncryptedSharedPreferences(context: Context): SharedPreferences {
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        return EncryptedSharedPreferences.create(
+            BuildConfig.SHARED_PREF_KEY,
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
 
     fun saveAssistant(context: Context, assistant: Assistant) {
-        val sharedPreferences = getSharedPreferences(context)
+        val sharedPreferences = getEncryptedSharedPreferences(context)
         val editor = sharedPreferences.edit()
         val gson = Gson()
         val assistantJson = gson.toJson(assistant)
@@ -22,17 +31,16 @@ object SharedPrefManager {
     }
 
     fun getAssistant(context: Context): Assistant? {
-        val sharedPreferences = getSharedPreferences(context)
+        val sharedPreferences = getEncryptedSharedPreferences(context)
         val gson = Gson()
         val assistantJson = sharedPreferences.getString(BuildConfig.ASSISTANT_KEY, null)
         return gson.fromJson(assistantJson, Assistant::class.java)
     }
 
     fun clearAssistant(context: Context) {
-        val sharedPreferences = getSharedPreferences(context)
+        val sharedPreferences = getEncryptedSharedPreferences(context)
         val editor = sharedPreferences.edit()
         editor.remove(BuildConfig.ASSISTANT_KEY)
         editor.apply()
     }
-
 }
